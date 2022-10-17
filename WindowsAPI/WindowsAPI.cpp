c6d6cc654bb2ea28eea66a2940a1231ace5cb49f
eazuooz <aquarius4785@gmail.com>
2022-10-17 19:48:35 +0900

    "비쥬얼 스튜디오 깃허브 테스트"

--- WindowsAPI/WindowsAPI.cpp
+++ WindowsAPI/WindowsAPI.cpp
@@ -26,7 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: 여기에 코드를 입력합니다.
-
+    // 깃허브 테스트 코드입니다.
+    // 
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_WINDOWSAPI, szWindowClass, MAX_LOADSTRING);
